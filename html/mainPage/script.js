83a83088a8deeea98c7059ae9d1274edabc57b8b
Heath123 <heath.mitchell27@gmail.com>
2020-05-15 15:50:23 +0100

    Add padding for offscreen packets and change max onscreen packets to 300

--- html/mainPage/script.js
+++ html/mainPage/script.js
@@ -2,6 +2,7 @@ const { ipcRenderer } = require('electron');
 // const escapeHtml = require('escape-html'); Already defined in my custonmised version of jsonTree (I just added HTML escaping)
 
 var currentPacket = undefined;
+var currentPacketType = undefined;
 var hiddenPackets = ["update_time", "position", "position", "keep_alive", "keep_alive", "rel_entity_move", "position_look", "look", "position_look", "map_chunk", "update_light", "entity_action", "entity_update_attributes", "unload_chunk", "unload_chunk", "update_view_position", "entity_metadata"];
 
 var allPackets = [];
@@ -51,7 +52,7 @@ function refreshPackets() {
   packetsToAdd = [];
   var i;
   var packetsAdded = 0;
-  for (i = allPackets.length - 1; packetsAdded < 100 && !(i == -1); i--) {
+  for (i = allPackets.length - 1; packetsAdded < 300 && !(i == -1); i--) {
     if (!hiddenPackets.includes(allPackets[i].meta.name)) {
       packetsToAdd.push(allPackets[i]);
       packetsAdded++;
@@ -68,15 +69,16 @@ function addPacketToDOM(packet) {
     updateHidden();
     return;
   }
-  if (packetlist.childElementCount > 100) {
+  if (packetlist.childElementCount > 300) {
    packetlist.removeChild(packetlist.childNodes[0]);
    offScreenCount += 1;
   }
-  packetlist.innerHTML += `<li id="packet${allPackets.length - 1}" onclick="packetClick(${allPackets.length - 1})" class="packet ${packet.direction}">
+  packetlist.innerHTML += `<li id="packet${packet.uid}" onclick="packetClick(${packet.uid})" class="packet ${packet.direction}">
                <span class="id">${escapeHtml(packet.hexIdString)}</span>
                <span class="name">${escapeHtml(packet.meta.name)}</span>
                <span class="data">${escapeHtml(trimData(packet.data))}</span>
              </li>`; // TODO: Fix this mess
+  packetlist.style.paddingTop =  offScreenCount * 30 + "px"; // TODO: Make it so you can view these packets
   if (wasScrolledToBottom) {
     packetlist.parentElement.scrollTop = packetlist.parentElement.scrollHeight;
   }
@@ -91,6 +93,7 @@ function addPacketToDOM(packet) {
 ipcRenderer.on('packet', (event, arg) => {
   ipcMessage = JSON.parse(arg);
   allPackets.push(ipcMessage);
+  ipcMessage.uid = allPackets.length - 1;
   addPacketToDOM(ipcMessage);
 });
 
@@ -104,7 +107,7 @@ ipcRenderer.on('packet', (event, arg) => {
         if (hiddenPackets.includes(ipcMessage.packetName)) {
           break;
         }
-       if (packetlist.childElementCount > 100) {
+       if (packetlist.childElementCount > 300) {
          packetlist.removeChild(packetlist.childNodes[0]);
        }
        packetlist.innerHTML += `<li id="packet${ipcMessage.id}" onclick="packetClick(${ipcMessage.id})" class="packet ${ipcMessage.direction}">
@@ -126,6 +129,7 @@ function updateHidden() {
 
 function deselectPacket() {
   currentPacket = undefined;
+  currentPacketType = undefined;
   treeElement.firstElementChild.innerHTML = "No packet selected!";
   document.body.className = "noPacketSelected";
 }
@@ -148,6 +152,7 @@ function showAllPackets() {
 
 function packetClick(id) {
   currentPacket = id;
+  currentPacketType = document.getElementById("packet" + id).children[1].innerText;
   document.body.className = "packetSelected";
   // sidebar.innerHTML = '<div style="padding: 10px;">Loading packet data...</div>';
   tree.loadData(allPackets[id].data);
@@ -155,8 +160,7 @@ function packetClick(id) {
 
 function hideAll(id) {
   // TODO: Better solution
-  packetToHide = document.getElementById("packet" + id).children[1].innerText;
-  hiddenPackets.push(packetToHide);
+  hiddenPackets.push(currentPacketType);
   deselectPacket();
   refreshPackets();
 }
