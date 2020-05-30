d4f585a5bac90751eeda462826c9716b427bb16a
Heath123 <heath.mitchell27@gmail.com>
2020-05-30 21:49:06 +0100

    Tweak data preview for Bedrock

--- html/mainPage/script.js
+++ html/mainPage/script.js
@@ -35,21 +35,25 @@ var tree = jsonTree.create({}, treeElement);
 treeElement.firstElementChild.innerHTML = "No packet selected!"
 
 function trimData(data) { // Function to trim the size of stringified data for previews
-  newData = Object.assign({}, data);
-  Object.entries(newData).forEach(function(entry) {
-    try {
-      if (JSON.stringify(entry[1]).length > 15) {
-        if (typeof entry[1] == "number") {
-          newData[entry[0]] = Math.round((entry[1] + Number.EPSILON) * 100) / 100
-        } else {
-          newData[entry[0]] = "...";
+  if (proxyCapabilities.jsonData) {
+    newData = Object.assign({}, data);
+    Object.entries(newData).forEach(function(entry) {
+      try {
+        if (JSON.stringify(entry[1]).length > 15) {
+          if (typeof entry[1] == "number") {
+            newData[entry[0]] = Math.round((entry[1] + Number.EPSILON) * 100) / 100
+          } else {
+            newData[entry[0]] = "...";
+          }
         }
-      }
-    } catch(err) {
+      } catch(err) {
 
-    }
-  });
-  newData = JSON.stringify(newData);
+      }
+    });
+    newData = JSON.stringify(newData);
+  } else {
+    newData = data.data;
+  }
   if (newData.length > 750) {
     newData = newData.slice(0, 750);
   }
