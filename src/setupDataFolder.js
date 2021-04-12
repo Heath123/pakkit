ebae28663fe26ab1ec2dd5becf54cf192d2fd9ed
Heath123 <heath.mitchell27@gmail.com>
2021-04-12 21:11:49 +0100

    Fix console message

--- src/setupDataFolder.js
+++ src/setupDataFolder.js
@@ -6,9 +6,9 @@ function copyIfNotMatches (src, dest) {
     fs.copyFileSync(src, dest)
   } else {
     const packagedHash = md5File.sync(src)
-    console.log('The MD5 hash of the packaged', src.split('/')[src.split('/').length], 'is', packagedHash)
+    console.log('The MD5 hash of the packaged', src.split('/')[src.split('/').length - 1], 'is', packagedHash)
     const savedHash = md5File.sync(dest)
-    console.log('The MD5 hash of the saved', src.split('/')[src.split('/').length], 'is', savedHash)
+    console.log('The MD5 hash of the saved', src.split('/')[src.split('/').length - 1], 'is', savedHash)
     if (packagedHash === savedHash) {
       console.log('Match! Not copying.')
     } else {
